eb4c659b7aa18b493781368d45a7194038fb54b6
dave101 <dave101@gmail.com>
2023-01-09 20:07:00 +0000

    new changes to 2nd script

--- 2ndscript.sh
+++ 2ndscript.sh
@@ -6,5 +6,13 @@ read x
 echo "how old are you"
 read y 
 
-echo "your age is expo $x + $y"
+echo "your age is `expr $x + $y` " 
+
+echo "what is number"
+read p
+
+echo "times number"
+read q
+
+echo " $p times $q is `expr $p \* $q` "
 
