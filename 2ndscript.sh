384c2fc3ba9ff698e2f9740b27123f0380397af5
dave101 <dave101@gmail.com>
2023-03-14 18:36:27 +0000

    expression now fixed

--- 2ndscript.sh
+++ 2ndscript.sh
@@ -6,5 +6,13 @@ read x
 echo "how old are you"
 read y 
 
-echo "your age is expo $x + $y"
+echo "your age is `expr $x + $y`"
+
+echo "what is your new age"
+read a
+
+echo "what is your child's age"
+read b 
+
+echo "you gave birth to your child when you were `expr $a - $b`"
 
